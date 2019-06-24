cca6528e4ceb0d6ab1d2a608c029cd81f006b09a
Alex Lee <alex@uniteus.com>
2019-06-24 10:03:31 -0400

    Reducers and Sagas

--- src/index.js
+++ src/index.js
@@ -4,7 +4,31 @@ import axios from 'axios'
 import { configure } from 'radiks';
 import { UserSession, AppConfig } from 'blockstack';
 import { BrowserRouter } from 'react-router-dom'
+
+// Redux Configs
+import { Provider } from 'react-redux'
+import createSagaMiddleware from 'redux-saga'
+import { createStore, applyMiddleware, compose } from 'redux'
+import reducers from './reducers'
+
+// Relative Paths
+import mySaga from './sagas'
 import Index from './pages/index';
+
+const sagaMiddleware = createSagaMiddleware()
+
+const configureStore = (initialState) => {
+  const store = createStore(
+    reducers,
+    initialState,
+    compose(
+      applyMiddleware(sagaMiddleware),
+      window.devToolsExtension ? window.devToolsExtension() : f => f
+    ),
+  )
+
+  return store
+}
 
 const setAxiosHeaders = () => {
   axios.defaults.baseURL = process.env.NODE_ENV === 'development' ? process.env.REACT_APP_API_URL_DEV : process.env.REACT_APP_API_URL_PROD
@@ -16,15 +40,19 @@ const userSession = new UserSession({
 
 const apiServer = process.env.NODE_ENV === 'development' ? process.env.REACT_APP_API_URL_DEV : process.env.REACT_APP_API_RADIKS_PROD
 
+const store = configureStore()
+sagaMiddleware.run(mySaga)
+
 setAxiosHeaders()
 configure({
   apiServer,
   userSession
 });
 
-
 ReactDOM.render((
-  <BrowserRouter>
-    <Index />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Index />
+    </BrowserRouter>
+  </Provider>
 ), document.getElementById('root'));
