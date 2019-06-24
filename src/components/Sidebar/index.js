8dd7707ad6ac2835f79258c32cb69e9b03991510
Alex Lee <alex@uniteus.com>
2019-06-24 19:17:43 -0400

    More adjustments with resizing

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -5,12 +5,23 @@ import {
   Menu,
   Layout,
 } from 'antd';
+import useReactRouter from 'use-react-router'
 
 const {
   Sider,
 } = Layout
 
 function IrSidebar() {
+  const { history } = useReactRouter();
+
+  const onClick = (value) => {
+    if (value.key === 'resize') {
+      return history.push('/')
+    }
+
+    return history.push(`/${value.key}`)
+  }
+
   return (
     <Sider
       breakpoint="lg"
@@ -23,19 +34,19 @@ function IrSidebar() {
         css={css`
           background: inherit;
         `}
-        onClick={() => console.log('clicking')}
-        defaultSelectedKeys={['1']}
+        onClick={onClick}
+        defaultSelectedKeys={['resize']}
       >
         <Menu.Item
           css={css`
             margin-top: 0 !important;
           `}
-          key="1"
+          key="resize"
         >
           <Icon type="upload" />
           <span>Resize</span>
         </Menu.Item>
-        <Menu.Item key="2">
+        <Menu.Item key="files">
           <Icon type="file" />
           <span>My files</span>
         </Menu.Item>
