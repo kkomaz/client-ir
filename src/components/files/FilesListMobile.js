2cb596dcd82ad1a3616149050f11d97410652e3d
Alex Lee <alex@uniteus.com>
2019-07-04 12:09:32 -0400

    Adjust for mobile

--- src/components/files/FilesListMobile.js
+++ src/components/files/FilesListMobile.js
@@ -2,8 +2,8 @@
 import { jsx, css } from '@emotion/core'
 import PropTypes from 'prop-types'
 import _ from 'lodash'
-import { useCallback } from 'react'
-import { useDispatch } from 'react-redux'
+import { useCallback, useContext } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { deleteFile } from 'actions/file'
 import {
   Button,
@@ -11,12 +11,16 @@ import {
   Modal,
 } from 'antd'
 import { saveAs } from 'file-saver'
+import { UserContext } from 'components/UserProvider'
 
 const { confirm } = Modal
 
 function FilesListMobile(props) {
   const { files } = props
   const dispatch = useDispatch()
+  const blobs = useSelector(state => state.file.blobs)
+  const userContext = useContext(UserContext)
+  const { userSession } = userContext.state.sessionUser
 
   const saveLocally = (file) => {
     saveAs(file.attrs.blob, file.attrs.name)
@@ -25,9 +29,9 @@ function FilesListMobile(props) {
   const requestDeleteFile = useCallback(
     file => {
       return dispatch(
-        deleteFile(file)
+        deleteFile(file, userSession)
       )
-    }, [dispatch]
+    }, [dispatch, userSession]
   )
 
   const showDeleteConfirm = (file) => {
@@ -57,16 +61,24 @@ function FilesListMobile(props) {
       dataIndex: 'image',
       key: 'image',
       render: file => (
-        <img
-          src={file.attrs.blob}
+        <div
+          className={`file-${file._id}`}
           alt="resize"
           css={css`
-            background: url("${file.attrs.blob_lq}"));
-            background-repeat: no-repeat, no-repeat;
-            width: 100px;
-            height: 100px;
+            background-image: url("${file.attrs.blob_lq}");
+            width: 50px;
+            height: 50px;
           `}
-        />
+        >
+          <img
+            css={css`
+              width: 50px;
+              height: 50px;
+            `}
+            src={blobs[file.attrs.blob_id]}
+            alt=""
+          />
+        </div>
       )
     },
     {
