483eb1179f85d8524d59409908225cfc388294be
Alex Lee <alex@uniteus.com>
2019-08-19 20:59:48 -0400

    Clean up model

--- src/models/file.js
+++ src/models/file.js
@@ -4,29 +4,8 @@ export default class File extends Model {
   static className = 'File'
 
   static schema = {
-    name: {
-      type: String,
-    },
-    blob: {
-      type: String,
-    },
-    blob_lq: {
-      type: String,
-    },
     blob_id: {
       type: String,
-    },
-    max_height: {
-      type: Number,
-    },
-    max_width: {
-      type: Number,
-    },
-    new_height: {
-      type: Number,
-    },
-    new_width: {
-      type: Number,
     }
   }
 }
